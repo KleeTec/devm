7feb7239f56b31c505e20162f06b435f2f66f2b0
MrKleeblatt <mrkleeblatt@gmail.com>
2023-06-15 22:06:45 +0200

    screw automatic parsing, I write my own parser lol

--- devm.ts
+++
@@ -1,3 +0,0 @@
-import * as Cmd from "./modules/cmd/mod.ts";
-const args = Cmd.parse(Deno.args) as Cmd.Args;
-console.log(args);
--- modules/cmd/mod.ts
+++
@@ -1,6 +0,0 @@
-export { parse } from "$flags";
-
-type InstallOptions = { i: string } & { install: string };
-type RemoveOptions = { r: string } & { remove: string };
-
-export type Args = InstallOptions & RemoveOptions & { _: string[] };
