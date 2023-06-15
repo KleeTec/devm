fa524a5bd4b1a5841127a893c0a89dae3b769aa5
MrKleeblatt <mrkleeblatt@gmail.com>
2023-06-15 22:07:15 +0200

    different code structure

--- modules/install/mod.ts
+++
@@ -1,8 +0,0 @@
-export class InstallOptions {
-	author!: string;
-	name!: string;
-	version!: string;
-	repo: URL = new URL("");
-}
-export async function install(options: InstallOptions): Promise<void> {
-}
